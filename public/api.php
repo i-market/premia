f4579e7fa6d37c5e7d4a66500a7c0825e060ea7f
Artem Novikov <novikov@i-market.ru>
2016-12-17 22:34:02 +0300

    `message` mail

--- public/api.php
+++ public/api.php
@@ -5,18 +5,33 @@ require_once $autoload;
 
 class App {
     const ADMIN_EMAIL = 'novikov@i-market.ru';
+
+    static function newMail() {
+        $mail = new PHPMailer();
+        $mail->CharSet = 'UTF-8';
+        $mail->setFrom('postmaster@domodedovo.nichost.ru', 'Четыре сезона');
+        $mail->addAddress(App::ADMIN_EMAIL);
+        return $mail;
+    }
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $fn = $_POST['fn'];
     if ($fn === 'callback-request') {
         $params = $_POST;
-        $mail = new PHPMailer();
-        $mail->CharSet = 'UTF-8';
-        $mail->setFrom('postmaster@domodedovo.nichost.ru', 'Четыре сезона');
-        $mail->addAddress(App::ADMIN_EMAIL);
+        $mail = App::newMail();
         $mail->Subject = 'На сайте заказали обратный звонок';
-        $mail->Body = 'Имя: '.$params['name'].PHP_EOL.'Телефон: '.$params['phone'].PHP_EOL.'Сообщение: '.$params['message'];
+        $mail->Body = 'Имя: '.$params['name'].PHP_EOL
+            .'Телефон: '.$params['phone'].PHP_EOL
+            .'Сообщение: '.$params['message'];
+        $mail->send();
+    } elseif ($fn === 'message') {
+        $params = $_POST;
+        $mail = App::newMail();
+        $mail->Subject = 'С сайта отправлено письмо';
+        $mail->Body = 'Имя: '.$params['name'].PHP_EOL
+            .'Email: '.$params['email'].PHP_EOL
+            .'Сообщение: '.$params['message'];
         $mail->send();
     }
 }
