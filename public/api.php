ffb5cc9f1e2c9d63cf86fc85c370e6ed7ab7c312
Artem Novikov <novikov@i-market.ru>
2016-12-19 16:20:37 +0300

    send email separately for each address

--- public/api.php
+++ public/api.php
@@ -4,12 +4,13 @@ $autoload = $_SERVER['DOCUMENT_ROOT'].'/private/vendor/autoload.php';
 require_once $autoload;
 
 class App {
-    static function newMail() {
+    static $emailTo = array('surovets@mspdom.ru', 'bezin@i-market.ru');
+
+    static function newMail($emailTo) {
         $mail = new PHPMailer();
         $mail->CharSet = 'UTF-8';
         $mail->setFrom('postmaster@domodedovo.nichost.ru', 'Четыре сезона');
-        $mail->addAddress('surovets@mspdom.ru');
-        $mail->addAddress('bezin@i-market.ru');
+        $mail->addAddress($emailTo);
         return $mail;
     }
 }
@@ -18,28 +19,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $fn = $_POST['fn'];
     if ($fn === 'callback-request') {
         $params = $_POST;
-        $mail = App::newMail();
-        $mail->Subject = 'На сайте заказали обратный звонок';
-        $mail->Body = 'ФИО: '.$params['name'].PHP_EOL
-            .'Телефон: '.$params['phone'].PHP_EOL
-            .'Сообщение: '.$params['message'];
-        $mail->send();
+        foreach (App::$emailTo as $email) {
+            $mail = App::newMail($email);
+            $mail->Subject = 'На сайте заказали обратный звонок';
+            $mail->Body = 'ФИО: '.$params['name'].PHP_EOL
+                .'Телефон: '.$params['phone'].PHP_EOL
+                .'Сообщение: '.$params['message'];
+            $mail->send();
+        }
     } elseif ($fn === 'message') {
         $params = $_POST;
-        $mail = App::newMail();
-        $mail->Subject = 'С сайта отправлено письмо';
-        $mail->Body = 'ФИО: '.$params['name'].PHP_EOL
-            .'Почта: '.$params['email'].PHP_EOL
-            .'Сообщение: '.$params['message'];
-        $mail->send();
+        foreach (App::$emailTo as $email) {
+            $mail = App::newMail($email);
+            $mail->Subject = 'С сайта отправлено письмо';
+            $mail->Body = 'ФИО: '.$params['name'].PHP_EOL
+                .'Почта: '.$params['email'].PHP_EOL
+                .'Сообщение: '.$params['message'];
+            $mail->send();
+        }
     } elseif ($fn === 'rent') {
         $params = $_POST;
-        $mail = App::newMail();
-        $mail->Subject = 'На сайте запросили форму на аренду';
-        $mail->Body = 'ФИО: '.$params['name'].PHP_EOL
-            .'Телефон: '.$params['phone'].PHP_EOL
-            .'Почта: '.$params['email'].PHP_EOL
-            .'Тип помещения: '.$params['type'];
-        $mail->send();
+        foreach (App::$emailTo as $email) {
+            $mail = App::newMail($email);
+            $mail->Subject = 'На сайте запросили форму на аренду';
+            $mail->Body = 'ФИО: ' . $params['name'] . PHP_EOL
+                . 'Телефон: ' . $params['phone'] . PHP_EOL
+                . 'Почта: ' . $params['email'] . PHP_EOL
+                . 'Тип помещения: ' . $params['type'];
+            $mail->send();
+        }
     }
 }
