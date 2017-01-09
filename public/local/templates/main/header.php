d3c79556fc0180bb2ba7298bf1f2b8b3fe80da96
Artem Novikov <novikov@i-market.ru>
2017-01-10 00:10:30 +0300

    refactor base.twig a bit

--- public/local/templates/main/header.php
+++ public/local/templates/main/header.php
@@ -46,7 +46,15 @@ $scripts = array(
     'vendor/slick.min.js',
     'script.js'
 );
+$menu = array_map(function($item) use (&$APPLICATION) {
+    $item['is_active'] = $item['uri'] === $APPLICATION->GetCurDir();
+    return $item;
+}, array(
+    'rent' => array('uri' => SITE_DIR.'rent/'),
+    'for_customers' => array('uri' => SITE_DIR.'for-customers/')
+));
 View::showLayoutHeader(array(
+    'menu' => $menu,
     'scripts' => $scripts,
     'copyright_year' => date('Y'),
     'include_tracking_scripts' => App::env() !== Env::DEV
