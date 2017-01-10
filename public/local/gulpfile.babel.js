224f49e98e38e7f96b23dc38f73cef2438e930ff
Artem Novikov <novikov@i-market.ru>
2017-01-10 12:40:45 +0300

    gulp mockup delegated build task

--- public/local/gulpfile.babel.js
+++ public/local/gulpfile.babel.js
@@ -40,11 +40,11 @@ gulp.task('test:e2e', () => {
   const tests = ['out/e2e-tests/test.js'];
   // pass the arguments to casper
   const args = _.drop(process.argv, 3);
-  const casperProc = child.spawn('casperjs', ['test'].concat(tests).concat(args));
-  casperProc.stdout.on('data', function (data) {
+  const proc = child.spawn('casperjs', ['test'].concat(tests).concat(args));
+  proc.stdout.on('data', function (data) {
     util.log('CasperJS:', data.toString().slice(0, -1)); // Remove \n
   });
-  casperProc.on('close', function (code) {
+  proc.on('close', function (code) {
     const success = code === 0; // Will be 1 in the event of failure
     // Do something with success here
   });
@@ -54,4 +54,22 @@ gulp.task('dev', ['sass', 'css', 'js', 'browser-sync'], () => {
   gulp.watch('mockup/css/*.scss', ['sass']);
   gulp.watch('mockup/js/**/*.js', ['js']);
   gulp.watch(['templates/main/**/*.php', 'templates/main/**/*.twig']).on('change', browserSync.reload);
+});
+
+gulp.task('build', () => {
+  process.chdir('mockup');
+  const proc = child.exec('gulp sass && gulp css && gulp dist');
+  process.chdir('..');
+  proc.stdout.on('data', function (data) {
+    util.log('mockup:', data.toString().slice(0, -1));
+  });
+  proc.on('close', function (code) {
+    const success = code === 0;
+    if (success) {
+      gulp.src('mockup/dist/**/*.{js,css,png,jpg,jpeg}')
+        .pipe(gulp.dest('templates/main/assets'));
+      gulp.src('assets/**/*')
+        .pipe(gulp.dest('templates/main/assets'));
+    }
+  });
 });
