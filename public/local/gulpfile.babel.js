baa8ae085e2ca020f5ce673366cfa6f5f7a14f7c
Artem Novikov <novikov@i-market.ru>
2017-01-11 16:18:47 +0300

    refactor gulp build

--- public/local/gulpfile.babel.js
+++ public/local/gulpfile.babel.js
@@ -3,28 +3,62 @@ import babel from 'gulp-babel';
 import revAll from 'gulp-rev-all';
 import sass from 'gulp-sass';
 import util from 'gulp-util';
+import clean from 'gulp-clean';
 import browserSync from 'browser-sync';
 import child from 'child_process';
 import _ from 'lodash';
+import config from './config.json';
 
-const config = {
-  mockupBuildCommand: 'gulp sass && gulp css && gulp dist'
-};
 const paths = {
   template: 'templates/main'
 };
-paths.dest = `${paths.template}/assets`;
+paths.dist = `${paths.template}/assets`;
 
-gulp.task('sass', () => {
-  return gulp.src('mockup/css/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest(`${paths.dest}/css`))
-    .pipe(browserSync.stream());
+function delegateMockupBuild(cb) {
+  process.chdir('mockup');
+  const proc = child.exec(config.mockup.buildCommand);
+  process.chdir('..');
+  proc.stdout.on('data', function (data) {
+    util.log('mockup:', data.toString().slice(0, -1));
+  });
+  proc.on('close', cb);
+}
+
+gulp.task('build:clean', () => {
+  gulp.src(paths.dist)
+    .pipe(clean());
 });
 
-gulp.task('js', () => {
-  return gulp.src('mockup/js/**/*.js')
-    .pipe(gulp.dest(`${paths.dest}/js`));
+gulp.task('build:mockup', () => {
+  delegateMockupBuild((code) => {
+    const isSuccess = code === 0;
+    if (isSuccess) {
+      gulp.src(`mockup/${config.mockup.buildGlob}`)
+        .pipe(gulp.dest(paths.dist));
+    }
+  });
+});
+
+gulp.task('build:js', () => {
+  gulp.src('assets/js/**/*.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest(`${paths.dist}/js`));
+});
+
+gulp.task('build:images', () => {
+  gulp.src('assets/images/**', {base: 'assets'})
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('build', ['build:mockup', 'build:js', 'build:images']);
+
+gulp.task('dev:sass', () => {
+  return gulp.src('mockup/css/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest(`${paths.dist}/css`))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('browser-sync', () => {
@@ -32,6 +66,13 @@ gulp.task('browser-sync', () => {
     proxy: 'bitrix.localhost', // TODO config
     open: false
   });
+});
+
+gulp.task('dev', ['build', 'browser-sync'], () => {
+  gulp.watch('mockup/css/*.scss', ['dev:sass']);
+  // TODO watch mockup js
+  gulp.watch('assets/js/**/*.js', ['build:js']);
+  gulp.watch([`${paths.template}/**/*.php`, `${paths.template}/**/*.twig`]).on('change', browserSync.reload);
 });
 
 gulp.task('test:e2e', () => {
@@ -53,38 +94,3 @@ gulp.task('test:e2e', () => {
   });
 });
 
-gulp.task('dev', ['sass', 'js', 'browser-sync'], () => {
-  gulp.watch('mockup/css/*.scss', ['sass']);
-  gulp.watch('mockup/js/**/*.js', ['js']);
-  gulp.watch([`${paths.template}/**/*.php`, `${paths.template}/**/*.twig`]).on('change', browserSync.reload);
-});
-
-const build = {
-  combine: () => {
-    gulp.src('mockup/dist/**/*.{js,css,png,jpg,jpeg}')
-      .pipe(gulp.dest(paths.dest))
-      .pipe(revAll.revision())
-      .pipe(gulp.dest(paths.dest))
-      .pipe(revAll.manifestFile())
-      .pipe(gulp.dest(paths.dest));
-    gulp.src('assets/**/*')
-      .pipe(gulp.dest(paths.dest));
-  }
-};
-
-gulp.task('build:combine', build.combine);
-
-gulp.task('build', () => {
-  process.chdir('mockup');
-  const proc = child.exec(config.mockupBuildCommand);
-  process.chdir('..');
-  proc.stdout.on('data', function (data) {
-    util.log('mockup:', data.toString().slice(0, -1));
-  });
-  proc.on('close', function (code) {
-    const success = code === 0;
-    if (success) {
-      build.combine();
-    }
-  });
-});
