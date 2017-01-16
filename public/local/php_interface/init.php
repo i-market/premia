29f92febcc1b0fa8b697001864a10df1acc584a1
Artem Novikov <novikov@i-market.ru>
2017-01-16 11:44:29 +0300

    remove phpdotenv dead code

--- public/local/php_interface/init.php
+++ public/local/php_interface/init.php
@@ -1,17 +1,7 @@
 <?php
 
-use Dotenv\Dotenv;
-
-$localDir = $_SERVER['DOCUMENT_ROOT'].'/local';
-
 // composer
-require $localDir.'/vendor/autoload.php';
-
-// load environment variables from .env to getenv(), $_ENV and $_SERVER
-if (file_exists($localDir.'/.env')) {
-    $dotenv = new Dotenv($localDir);
-    $dotenv->load();
-}
+require $_SERVER['DOCUMENT_ROOT'].'/local/vendor/autoload.php';
 
 // https://github.com/ajgarlag/AjglBreakpointTwigExtension/blob/0dfa4f0ae3bbeb6c8036e3e6d6c204c43b090155/src/BreakpointExtension.php
 class BreakpointExtension extends Twig_Extension
@@ -44,7 +34,6 @@ class BreakpointExtension extends Twig_Extension
 // TODO tools.twig onAfterTwigTemplateEngineInited
 $twig = \Maximaster\Tools\Twig\TemplateEngine::getInstance()->getEngine();
 $twig->addExtension(new BreakpointExtension());
-// TODO rename to `asset` for consistency
 $twig->addFunction(new Twig_SimpleFunction('asset', 'Hendrix\View::asset'));
 $twig->addFunction(new Twig_SimpleFunction('partial', 'Hendrix\View::partial'));
 $twig->addFunction(new Twig_SimpleFunction('path', 'Hendrix\View::path'));
