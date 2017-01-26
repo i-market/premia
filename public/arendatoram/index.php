55517b27fc205802e29c4033ce9124349cdbd75a
Artem Novikov <novikov@i-market.ru>
2017-01-26 12:23:03 +0300

    .rental_offers modal content

--- public/arendatoram/index.php
+++ public/arendatoram/index.php
@@ -3,6 +3,10 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Арендаторам");
 ?>
 
-<?= \App\View::twig()->render('/arendatoram/index.twig') ?>
+<?= \App\View::twig()->render('/arendatoram/index.twig', array(
+    'rental_offers' => array('korpus-4.jpg', '1-1.png', '1-2.png'),
+    'rental_offers_2' => array('korpus-11.jpg', 'korpus-12.jpg', 'korpus-13.jpg'),
+    'rental_offers_3' => array('offices-1.jpg', 'offices-2.jpg', 'abk.jpg', '2-floor-abk.jpg')
+)) ?>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
