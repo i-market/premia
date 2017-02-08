5d437af6fc48acf5eed5aaa3a4bf645099af08b0
Artem Novikov <novikov@i-market.ru>
2017-02-08 11:15:52 +0300

    set correct http status for 404 errors

--- public/404.php
+++ public/404.php
@@ -1,7 +1,11 @@
 <?
+use Bitrix\Main\Application;
+
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("404");
 $APPLICATION->SetPageProperty(\App\PageProperty::LAYOUT, 'error.twig');
+$response = Application::getInstance()->getContext()->getResponse();
+$response->setStatus('404 Not Found');
 ?>
 
 <h1>Ошибка 404</h1>
