9b44c5ee426610f42e64663df7461a4d2a89d9c5
Artem Novikov <novikov@i-market.ru>
2017-02-10 18:02:41 +0300

    slider news.list

--- public/local/src/app.php
+++ public/local/src/app.php
@@ -8,6 +8,7 @@ class App {
     static function layoutContext() {
         return array(
             'main_menu' => self::renderMainMenu(),
+            'slider' => self::renderSlider(),
             'auth_modal' => Auth::renderAuthForm(),
             'signup_path' => Auth::signupPath(),
             'profile_path' => Auth::profilePath()
@@ -33,8 +34,75 @@ class App {
                 "USE_EXT" => "Y"
             )
         );
-return ob_get_clean();
+        return ob_get_clean();
     }
+
+    static function renderSlider() {
+        global $APPLICATION;
+        ob_start();
+        $APPLICATION->IncludeComponent(
+        	"bitrix:news.list",
+        	"slider",
+        	Array(
+        		"ACTIVE_DATE_FORMAT" => "j F Y",
+        		"ADD_SECTIONS_CHAIN" => "N",
+        		"AJAX_MODE" => "N",
+        		"AJAX_OPTION_ADDITIONAL" => "",
+        		"AJAX_OPTION_HISTORY" => "N",
+        		"AJAX_OPTION_JUMP" => "N",
+        		"AJAX_OPTION_STYLE" => "Y",
+        		"CACHE_FILTER" => "N",
+        		"CACHE_GROUPS" => "Y",
+        		"CACHE_TIME" => "36000000",
+        		"CACHE_TYPE" => "A",
+        		"CHECK_DATES" => "Y",
+        		"DETAIL_URL" => "",
+        		"DISPLAY_BOTTOM_PAGER" => "Y",
+        		"DISPLAY_DATE" => "Y",
+        		"DISPLAY_NAME" => "Y",
+        		"DISPLAY_PICTURE" => "Y",
+        		"DISPLAY_PREVIEW_TEXT" => "Y",
+        		"DISPLAY_TOP_PAGER" => "N",
+        		"FIELD_CODE" => array("", ""),
+        		"FILTER_NAME" => "",
+        		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+        		"IBLOCK_ID" => Iblock::SLIDER_ID,
+        		"IBLOCK_TYPE" => Iblock::CONTENT_TYPE,
+        		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+        		"INCLUDE_SUBSECTIONS" => "Y",
+        		"MESSAGE_404" => "",
+        		"NEWS_COUNT" => PHP_INT_MAX,
+        		"PAGER_BASE_LINK_ENABLE" => "N",
+        		"PAGER_DESC_NUMBERING" => "N",
+        		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+        		"PAGER_SHOW_ALL" => "N",
+        		"PAGER_SHOW_ALWAYS" => "N",
+        		"PAGER_TEMPLATE" => ".default",
+        		"PAGER_TITLE" => '',
+        		"PARENT_SECTION" => "",
+        		"PARENT_SECTION_CODE" => "",
+        		"PREVIEW_TRUNCATE_LEN" => "",
+        		"PROPERTY_CODE" => array("LINK"),
+        		"SET_BROWSER_TITLE" => "N",
+        		"SET_LAST_MODIFIED" => "N",
+        		"SET_META_DESCRIPTION" => "N",
+        		"SET_META_KEYWORDS" => "N",
+        		"SET_STATUS_404" => "N",
+        		"SET_TITLE" => "N",
+        		"SHOW_404" => "N",
+        		"SORT_BY1" => "ACTIVE_FROM",
+        		"SORT_BY2" => "SORT",
+        		"SORT_ORDER1" => "DESC",
+        		"SORT_ORDER2" => "ASC"
+        	)
+        );
+        return ob_get_clean();
+    }
+}
+
+class Iblock {
+    const CONTENT_TYPE = 'content';
+    const SLIDER_ID = 1;
 }
 
 class Auth {
