c355ac8c06be8ea5feefb0a747b8fab00d408575
Artem Novikov <novikov@i-market.ru>
2017-02-10 17:37:40 +0300

    main menu component stub

--- public/local/src/app.php
+++ public/local/src/app.php
@@ -7,10 +7,33 @@ use Core\View as v;
 class App {
     static function layoutContext() {
         return array(
+            'main_menu' => self::renderMainMenu(),
             'auth_modal' => Auth::renderAuthForm(),
             'signup_path' => Auth::signupPath(),
             'profile_path' => Auth::profilePath()
         );
+    }
+
+    static function renderMainMenu() {
+        global $APPLICATION;
+        ob_start();
+        $APPLICATION->IncludeComponent(
+            "bitrix:menu",
+            "main",
+            Array(
+                "ALLOW_MULTI_SELECT" => "N",
+                "CHILD_MENU_TYPE" => "left",
+                "DELAY" => "N",
+                "MAX_LEVEL" => "2",
+                "MENU_CACHE_GET_VARS" => array(""),
+                "MENU_CACHE_TIME" => "3600",
+                "MENU_CACHE_TYPE" => "N",
+                "MENU_CACHE_USE_GROUPS" => "Y",
+                "ROOT_MENU_TYPE" => "top",
+                "USE_EXT" => "Y"
+            )
+        );
+return ob_get_clean();
     }
 }
 
