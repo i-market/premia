1d76910b67780b75ba0f982f4edd007a05eb0fa3
Artem Novikov <novikov@i-market.ru>
2017-02-14 13:26:26 +0300

    hero slider and partners sorting

--- public/local/src/Homepage.php
+++ public/local/src/Homepage.php
@@ -78,9 +78,9 @@ class Homepage {
         		"SET_STATUS_404" => "N",
         		"SET_TITLE" => "N",
         		"SHOW_404" => "N",
-        		"SORT_BY1" => "ACTIVE_FROM",
+        		"SORT_BY1" => "ID",
         		"SORT_BY2" => "SORT",
-        		"SORT_ORDER1" => "DESC",
+        		"SORT_ORDER1" => "ASC",
         		"SORT_ORDER2" => "ASC"
         	)
         );
