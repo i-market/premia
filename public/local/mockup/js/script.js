3c3dd6c574c7e375cc6f233b3aadfe6d97fbc399
Artem Novikov <novikov@i-market.ru>
2017-02-23 22:15:10 +0300

    [mockup] add the final version

--- public/local/mockup/js/script.js
+++ public/local/mockup/js/script.js
@@ -28,6 +28,50 @@ $(document).ready(function () {
   $('.hamburger').on('click', function () {
     $('.header, .footer, .content, body, html').toggleClass('open');
   });
+  // textarea
+  var maxLength = 15000;
+  $('textarea').each(function () {
+    $(this).keyup(function () {
+      var length = $(this).val().length;
+      var length = maxLength - length;
+      $(this).parent().find('.chars').text(length);
+    });
+    $(this).focus(function () {
+      $(this).parent().addClass('focus');
+      $(this).parent().find('.placeholder').css('color', '#0066cc');
+    });
+    $(this).focusout(function () {
+      $(this).parent().find('.placeholder').css('color', '#606778');
+      if ($(this).val() === '') {
+        $(this).parent().removeClass('focus');
+      } else {
+        $(this).parent().addClass('focus');
+      };
+    });
+  });
+  var div = document.querySelectorAll('.label_textarea');
+  var ta = document.querySelector('textarea');
+  div.forEach(function (itm, idx) {
+      var t = itm.children[0],
+        d = itm;
+      t.addEventListener('keydown', function () {
+        setTimeout(function () {
+          t.style.cssText = 'height:0px';
+          var height = Math.min(20 * 5, t.scrollHeight);
+          d.style.cssText = 'height:' + height + 'px';
+          t.style.cssText = 'height:' + height + 'px';
+        }, 0);
+      });
+    })
+    // табы
+  $(function () {
+    $('[data-tabLinks]').on('click', function () {
+      var targetNode = $('[data-tabContent=' + $(this).attr('data-tabLinks') + ']');
+      $(this).parent().find('[data-tabLinks]').removeClass('active').filter(this).addClass('active');
+      targetNode.parent().find('[data-tabContent]').hide().filter(targetNode).show();
+    });
+    $('[data-tabLinks]').parent().find('[data-tabLinks]:nth-child(1)').trigger('click');
+  });
   // слайдеры
   $('.slider').slick({
     arrows: false,
@@ -47,7 +91,7 @@ $(document).ready(function () {
           slidesToShow: 3,
           slidesToScroll: 1
         }
-    },{
+    }, {
         breakpoint: 580,
         settings: {
           slidesToShow: 1,
